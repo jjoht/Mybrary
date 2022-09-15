9f2d617475f1798faa47289355c56a30986ec9a2
jjoht <youhonka@gmail.com>
2022-09-15 14:13:23 +0300

    part two unfinished

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express()
 const expressLayouts = require("express-ejs-layouts")
 
 const indexRouter = require("./routes/index")
+const authorRouter = require("./routes/authors")
 
 app.set("view engine", "ejs")
 app.set("views", __dirname + "/views")
@@ -21,9 +22,9 @@ const db = mongoose.connection
 db.on("error", error => console.error(error))
 db.once("open", () => console.log("Connected to Mongoose"))
 
-
-
 app.use("/", indexRouter)
+app.use("/authors", authorRouter)
+
 
 app.listen(process.env.PORT || 3000)
 
